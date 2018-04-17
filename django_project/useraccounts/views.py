9a6182abc19b701f088ce5f8daad0fbfd94f8b00
Boney Bun <boney.bun@gmail.com>
2018-04-17 13:17:57 +0700

    validate form in user profile page
    check if required fields are empty.
    
    if so, show error message.
    
    fix #499

--- django_project/useraccounts/views.py
+++ django_project/useraccounts/views.py
@@ -1,1 +1,108 @@
 # Create your views here.
+from userena.decorators import secure_required
+from userena.utils import get_profile_model, get_user_profile
+from userena.forms import EditProfileForm
+from guardian.decorators import permission_required_or_403
+from django.contrib.auth import get_user_model
+from django.shortcuts import redirect, get_object_or_404
+from django.contrib import messages
+from userena import settings as userena_settings
+from django.core.urlresolvers import reverse
+from django.views.generic import TemplateView
+
+from userena import signals as userena_signals
+
+
+class ExtraContextTemplateView(TemplateView):
+    """ Add extra context to a simple template view """
+    extra_context = None
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ExtraContextTemplateView, self).get_context_data(*args, **kwargs)
+        if self.extra_context:
+            context.update(self.extra_context)
+        return context
+
+    # this view is used in POST requests, e.g. signup when the form is not valid
+    post = TemplateView.get
+
+
+@secure_required
+@permission_required_or_403('change_profile', (get_profile_model(), 'user__username', 'username'))
+def profile_edit(request, username, edit_profile_form=EditProfileForm,
+                 template_name='useraccounts/profile_form.html', success_url=None,
+                 extra_context=None, **kwargs):
+    """
+    Edit profile.
+
+    Edits a profile selected by the supplied username. First checks
+    permissions if the user is allowed to edit this profile, if denied will
+    show a 404. When the profile is successfully edited will redirect to
+    ``success_url``.
+
+    :param username:
+        Username of the user which profile should be edited.
+
+    :param edit_profile_form:
+
+        Form that is used to edit the profile. The :func:`EditProfileForm.save`
+        method of this form will be called when the form
+        :func:`EditProfileForm.is_valid`.  Defaults to :class:`EditProfileForm`
+        from userena.
+
+    :param template_name:
+        String of the template that is used to render this view. Defaults to
+        ``userena/edit_profile_form.html``.
+
+    :param success_url:
+        Named URL which will be passed on to a django ``reverse`` function after
+        the form is successfully saved. Defaults to the ``userena_detail`` url.
+
+    :param extra_context:
+        Dictionary containing variables that are passed on to the
+        ``template_name`` template.  ``form`` key will always be the form used
+        to edit the profile, and the ``profile`` key is always the edited
+        profile.
+
+    **Context**
+
+    ``form``
+        Form that is used to alter the profile.
+
+    ``profile``
+        Instance of the ``Profile`` that is edited.
+
+    """
+    user = get_object_or_404(get_user_model(), username__iexact=username)
+
+    profile = get_user_profile(user=user)
+
+    user_initial = {'first_name': user.first_name,
+                    'last_name': user.last_name}
+
+    form = edit_profile_form(instance=profile, initial=user_initial)
+
+    if request.method == 'POST':
+        form = edit_profile_form(request.POST, request.FILES, instance=profile,
+                                 initial=user_initial)
+
+        if form.is_valid():
+            profile = form.save()
+
+            if userena_settings.USERENA_USE_MESSAGES:
+                messages.success(request, ('Your profile has been updated.'),
+                                 fail_silently=True)
+
+            if success_url:
+                # Send a signal that the profile has changed
+                userena_signals.profile_change.send(sender=None,
+                                                    user=user)
+                redirect_to = success_url
+            else: redirect_to = reverse('userena_profile_detail', kwargs={'username': username})
+            return redirect(redirect_to)
+
+    if not extra_context: extra_context = dict()
+    extra_context['form'] = form
+    extra_context['profile'] = profile
+    return ExtraContextTemplateView.as_view(template_name=template_name,
+                                            extra_context=extra_context)(request)
