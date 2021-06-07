40a383f80e87cc25d2b5775859e74c412d220d81
Faneva Andriamiadantsoa <fanevanjanahary@gmail.com>
2021-06-07 09:29:01 +0300

    add search serializers

---
+++ django_project/search/serializers.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from rest_framework import serializers
+from .models import SearchRecord
+
+
+class SearchRecordSerializer(serializers.ModelSerializer):
+    """
+    Serializer for search record.
+    """
+    user = serializers.HiddenField(
+        default=serializers.CurrentUserDefault()
+    )
+
+    class Meta:
+        model = SearchRecord
+        fields = [
+            'user',
+            'order',
+            'product',
+        ]
