5f562fc88a3f8ff067ac68ef4118f2f414404541
Faneva Andriamiadantsoa <fanevanjanahary@gmail.com>
2021-11-18 09:35:18 +0300

    change static files

--- django_project/core/base_static/js/form.js
+++ django_project/core/base_static/js/form.js
@@ -1,15 +1,3 @@
-var config = {
-    '.chosen-select'           : {},
-    '.chosen-select-deselect'  : {allow_single_deselect:true},
-    '.chosen-select-no-single' : {disable_search_threshold:10},
-    '.chosen-select-no-results': {no_results_text:'Oops, nothing found!'},
-    '.chosen-select-width'     : {width:"95%"}
-};
-
-for (var selector in config) {
-    $(selector).chosen(config[selector]);
-}
-
 $(document).ready(function () {
 
     var passwordMessage = '<div class="alert alert-info password-message" role="alert">' +
