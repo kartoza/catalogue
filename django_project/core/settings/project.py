6bdb2bb29646e573b9cdfcb2d59d0d1f01e08560
Faneva Andriamiadantsoa <fanevanjanahary@gmail.com>
2022-01-24 11:09:21 +0300

    remove unused code

--- django_project/core/settings/project.py
+++ django_project/core/settings/project.py
@@ -122,22 +122,6 @@ PIPELINE = {
         },
     },
     'JAVASCRIPT': {
-        # 'orderpage': {
-        #     'source_filenames': (
-        #         'js/libs/jquery/jquery-3.6.0.min.js',
-        #         'js/libs/openlayers-6.5.0/ol.js',
-        #         'js/libs/bootstrap-5.0.2/js/bootstrap.min.js',
-        #         'js/bootstrap-modal.js',
-        #         'js/bootstrap-modalmanager.js',
-        #         'js/map_layers.js',
-        #         # 'js/openlayers-plugins/ScaleBar.js',
-        #         # 'js/widget.sansaMap.js',
-        #         # 'js/widget.sansaCartLayer.js',
-        #         'js/widget.deliveryOptions.js',
-        #         'js/widget.nonSearchRecordsTable.js'
-        #     ),
-        #     'output_filename': 'js/orderpage.js',
-        # }
 
     }
 }
