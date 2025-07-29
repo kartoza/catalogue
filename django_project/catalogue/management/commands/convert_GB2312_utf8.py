9e0b29ef6283e2db7c36f0054a74ebac2113b0e6
Juanique Voogt <voogt001@gmail.com>
2025-07-29 17:45:31 +0200

    update cbers ingesting

--- django_project/catalogue/management/commands/convert_GB2312_utf8.py
+++ django_project/catalogue/management/commands/convert_GB2312_utf8.py
@@ -15,49 +15,57 @@ halt_on_error = True
 # source_path=('/home/web/django_project/data/CBERS/')
 
 class Command(BaseCommand):
-    args = 'path'
     help = 'Convert file from GB2312 encoding to UTF-8'
+
+    def add_arguments(self, parser):
+        parser.add_argument('path', type=str, help='Directory path containing XML files to convert')
 
     def handle(self, *args, **options):
         """Implementation for command
 
-        :param args: path for the main dir '/home/web/django_project/data/CBERS/'
-        :type args: str
+        :param args: Not used in modern Django
+        :param options: Command options including 'path'
+        :type options: dict
 
         :raises: IOError
 
         """
 
-        if len(args) < 1:
+        path = options['path']
+        if not path:
             self.stdout.write("Need argument for directory path "
                               "ex : /home/web/django_project/data/CBERS/")
             return
 
-        path = args[0]
-
         record_count = 0
         failed_record_count = 0
         print('Starting directory scan...')
+        print(f'Scanning path: {path}')
+        
+        # Check for both uppercase and lowercase XML extensions
+        xml_pattern1 = os.path.join(path, '*.XML')
+        xml_pattern2 = os.path.join(path, '*.xml')
+        
+        xml_files = glob.glob(xml_pattern1) + glob.glob(xml_pattern2)
+        print(f'Found {len(xml_files)} XML files')
+        
+        if not xml_files:
+            print('No XML files found in the directory!')
+            print(f'Checked patterns: {xml_pattern1} and {xml_pattern2}')
+            return
 
-        for myFolder in glob.glob(os.path.join(path, '*.XML')):
+        for xml_file in xml_files:
             record_count += 1
             try:
-                # Get the folder name
-                product_folder = os.path.split(myFolder)[-1]
-                # print product_folder
-
-                # Find the first and only xml file in the folder
-                # search_path = os.path.join(str(myFolder), '*.XML')
-                # print search_path
-                xml_file = glob.glob(myFolder)[0]
+                # Get the file name
                 filename = os.path.basename(xml_file)
                 print("Converting {} ....".format(filename))
                 pattern = 'GB2312'
                 subst = 'UTF-8'
 
                 fh, abs_path = mkstemp()
-                with open(abs_path, 'w') as new_file:
-                    with open(xml_file) as old_file:
+                with open(abs_path, 'w', encoding='utf-8') as new_file:
+                    with open(xml_file, 'r', encoding='gb2312') as old_file:
                         for line in old_file:
                             new_file.write(line.replace(pattern, subst))
                 close(fh)
@@ -67,10 +75,10 @@ class Command(BaseCommand):
                 move(abs_path, xml_file)
 
             except Exception as e:
-                print('Error when want to convert! : %s' % product_folder)
+                print('Error when want to convert! : %s' % filename)
                 failed_record_count += 1
                 if halt_on_error:
-                    print(e.message)
+                    print(str(e))
                     break
                 else:
                     continue
